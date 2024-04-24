b84c3ae97641b0d911a495f348d800335954870f
Jose Campillo <jcampillo1207@gmail.com>
2024-04-24 10:37:15 -0600

    Changes

--- lib/route.ts
+++ lib/route.ts
@@ -13,6 +13,11 @@ export async function send(email: string) {
     react: KnootWelcome(),
   });
   if (data) {
+    resend.contacts.create({
+      email: email,
+      unsubscribed: false,
+      audienceId: "4121cecb-8808-471c-9a46-a056625b0fe9",
+    });
     return true;
   } else {
     console.log("error");
@@ -24,7 +29,11 @@ export async function sendForm() {
 
   const { data } = await resend.emails.send({
     from: "Knoott <soporte@knoott.com>",
-    to: ["jcampillo@intelloai.com", "angelamarreed@gmail.com", "hmexsen@intelloai.com"],
+    to: [
+      "jcampillo@intelloai.com",
+      "angelamarreed@gmail.com",
+      "hmexsen@intelloai.com",
+    ],
     subject: "Nueva respuesta de formulario",
     text: "Se agregó un nuevo evento al formulario de Knoott",
   });
